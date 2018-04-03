519ebe8323aed2884659dd6923cddaba77844a75
vch <cyh9426372898@gmail.com>
2018-04-03 23:14:37 +0800

    add new file

---
+++ Elementary/Popular Words.py
@@ -0,0 +1,31 @@
+import re
+
+
+def popular_words(text, words):
+    d = {}
+    for word in words:
+        d[word] = len(re.findall(word, text, re.I))
+    return d
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One,
+I had just begun.
+When I was Two,
+I was nearly new.
+''', ['i', 'was', 'three']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert popular_words('''
+When I was One,
+I had just begun.
+When I was Two,
+I was nearly new.
+''', ['i', 'was', 'three']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0
+    }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
