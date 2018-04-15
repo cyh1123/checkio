371c711769ed88b9295e3ecd116f12b2d219ca2d
vch <cyh9426372898@gmail.com>
2018-04-15 13:19:49 +0800

    some change

---
+++ GitHub/Currency_Style.py
@@ -0,0 +1,15 @@
+import re
+
+
+def checkio(string):
+    def repl(matchobj):
+        part1, part2 = matchobj.group(1), matchobj.group(2)
+        if part1:
+            return part1.replace('.', ',') + part2.replace(',', '.')
+        return part2.replace(',', '.')
+    newstring = re.sub(r'(?<=\$)(\d+\.)*(\d+,\d+)', repl, string)
+    return newstring
+
+if __name__ == '__main__':
+    assert checkio("Lot 192.001 costs $1.000,94.") == "Lot 192.001 costs $1,000.94."
+    assert checkio("$1,23 + $2,34 = $3,57.") == "$1.23 + $2.34 = $3.57."
